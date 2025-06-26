df879180f7e19478b2fce5667bedc713ff399355
awssrini <awssrini8@gmail.com>
2025-06-26 15:20:22 +0800

    updated s3 and dynamodb details in backend.tf file

--- jenkin-server-code/backend.tf
+++ jenkin-server-code/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "poc-srini-terraform-state-s3-bucket"
     region         = "ap-southeast-1"
     key            = "poc1/jenkin-server-code/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "poc-srini-terraform-locks"
     encrypt        = true
   }
   required_version = ">=0.13.0"
