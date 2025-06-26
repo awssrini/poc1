d72242a0560a3234a435c641670b94968f6a6e2a
awssrini <awssrini8@gmail.com>
2025-06-26 12:54:44 +0800

    corrected the resource name in backend/outputs.tf file

--- backend/outputs.tf
+++ backend/outputs.tf
@@ -1,14 +1,9 @@
-output "cluster_endpoint" {
-  description = "EKS cluster endpoint"
-  value       = module.eks.cluster_endpoint
+output "s3_bucket_name" {
+  value       = aws_s3_bucket.terraform_state.id
+  description = "The name of the S3 bucket"
 }
 
-output "cluster_name" {
-  description = "EKS cluster name"
-  value       = module.eks.cluster_name
-}
-
-output "vpc_id" {
-  description = "VPC ID"
-  value       = module.vpc.vpc_id
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_locks.id
+  description = "The name of the DynamoDB table"
 }
